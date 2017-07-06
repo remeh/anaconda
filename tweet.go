197433eb6d385b858195666fa5c0671b1ce76a74
remeh <remeh@remeh.fr>
2017-07-06 14:04:38 +0200

    Support for fulltext. Took from https://github.com/ChimeraCoder/anaconda/pull/158/commits/83a9117363f24d4f09963e150e900e2b1955f172

--- tweet.go
+++ tweet.go
@@ -9,11 +9,13 @@ type Tweet struct {
 	Contributors                []int64                `json:"contributors"`
 	Coordinates                 *Coordinates           `json:"coordinates"`
 	CreatedAt                   string                 `json:"created_at"`
+	DisplayTextRange            []int                  `json:"display_text_range"`
 	Entities                    Entities               `json:"entities"`
 	ExtendedEntities            Entities               `json:"extended_entities"`
 	FavoriteCount               int                    `json:"favorite_count"`
 	Favorited                   bool                   `json:"favorited"`
 	FilterLevel                 string                 `json:"filter_level"`
+	FullText                    string                 `json:"full_text"`
 	HasExtendedProfile          bool                   `json:"has_extended_profile"`
 	Id                          int64                  `json:"id"`
 	IdStr                       string                 `json:"id_str"`
